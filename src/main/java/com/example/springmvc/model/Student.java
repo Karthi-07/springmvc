33065a584cf310bae56b89e53b06ee7d692d4771
karthi-07 <karthikeyanc0105@gmail.com>
2024-03-13 15:23:47 +0530

    Model class written and checked

---
+++ src/main/java/com/example/springmvc/model/Student.java
@@ -0,0 +1,31 @@
+package com.example.springmvc.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int studid;
+    private String  studname;
+    private String studdept;
+    public int getStudid() {
+        return studid;
+    }
+    public void setStudid(int studid) {
+        this.studid = studid;
+    }
+    public String getStudname() {
+        return studname;
+    }
+    public void setStudname(String studname) {
+        this.studname = studname;
+    }
+    public String getStuddept() {
+        return studdept;
+    }
+    public void setStuddept(String studdept) {
+        this.studdept = studdept;
+    }
+}
