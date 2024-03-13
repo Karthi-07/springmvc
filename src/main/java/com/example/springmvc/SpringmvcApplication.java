d288c8425dfefe65ffa095e38f28ea75a2f2ce1b
karthi-07 <karthikeyanc0105@gmail.com>
2024-03-13 14:46:10 +0530

    set database properties

--- src/main/java/com/example/springmvc/SpringmvcApplication.java
+++ src/main/java/com/example/springmvc/SpringmvcApplication.java
@@ -1,11 +1,8 @@
 package com.example.springmvc;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringmvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
