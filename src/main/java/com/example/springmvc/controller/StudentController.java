284d1b9b53b670ba7a6e5bc702332c1b9b627396
karthi-07 <karthikeyanc0105@gmail.com>
2024-03-14 15:15:07 +0530

    get method works perfectly

--- src/main/java/com/example/springmvc/controller/StudentController.java
+++ src/main/java/com/example/springmvc/controller/StudentController.java
@@ -1,13 +1,22 @@
 package com.example.springmvc.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.springmvc.model.Student;
+import com.example.springmvc.repository.StudentRepository;
+import com.example.springmvc.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class StudentController {
+  @Autowired
+  private StudentService studentService;
+  @Autowired
+  private StudentRepository studentRepository;
   @GetMapping("/students")
-   public String getStudents(){
-
-      return "student-list";
+   public List<Student> getStudents(){
+      return studentRepository.findAll();
   }
 }
--- src/main/java/com/example/springmvc/repository/StudentRepository.java
+++ src/main/java/com/example/springmvc/repository/StudentRepository.java
@@ -2,11 +2,14 @@ package com.example.springmvc.repository;
 
 import com.example.springmvc.model.Student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-public interface StudentRepository extends JpaRepository {
+@Repository
+@Transactional
+public interface StudentRepository extends JpaRepository<Student,Integer>{
+ @Override
  List<Student> findAll();
-
 }
