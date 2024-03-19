b7c6184c886fbe73cd2f1baeb4e5a74ccbaeb0c3
karthi-07 <karthikeyanc0105@gmail.com>
2024-03-19 14:35:03 +0530

    test classes added

---
+++ src/test/java/com/example/springmvc/model/StudentTest.java
@@ -0,0 +1,7 @@
+package com.example.springmvc.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+}
---
+++ src/test/java/com/example/springmvc/service/StudentServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.springmvc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @Test
+    void getAllStudents() {
+    }
+
+    @Test
+    void getStudentById() {
+    }
+}
